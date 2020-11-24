513539b4392de23258695d789840f4f2c7e82314
Vikram <vikram.hwm@gmail.com>
2020-11-24 13:48:00 -0500

    Update DB_Connection to cluster

--- server.js
+++ server.js
@@ -18,29 +18,28 @@ app.use(express.static('public'))
 
 app.use('/', indexRouter)
 
-// require('dotenv/config')
 
 app.use(bodyParser.json());
 
 
 app.use(express.urlencoded({extended: false}))
 
-//Import ROUTES
-// const postRoute = require('./routes')
 
-// app.use('posts', postRoute);
+// mongoose.connect(process.env.DB_CONNECTION,{
+//     useNewUrlParser : true,
+//     useUnifiedTopolgy:true,
+// })
+// const db = mongoose.connection
+// db.on('error' , error => console.log(error))
+// db.once('open', ()=>console.log('Connected to DB'))
 
-
-
-// mongoose.connect(process.env.DB_CONNECTION, 
-//     {useNewUrlParser: true },
-//     ()=>console.log('connected to DB')
-// );
-
-mongoose.connect(process.env.DATABASE_URL,{
-    useNewUrlParser : true})
-const db = mongoose.connection
-db.on('error' , error => console.log(error))
-db.once('open', ()=>console.log('Connected to DB'))
+mongoose.connect(process.env.DB_CONNECTION, {
+useUnifiedTopology: true,
+useNewUrlParser: true,
+})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log("error");
+});
 
 app.listen(process.env.PORT || 3000)
